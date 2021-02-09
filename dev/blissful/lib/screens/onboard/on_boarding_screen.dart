51ae377c9fc0118e4f07698d47698f8d4d9c730e
Adson Henrique Moreira da Silva <adsonhenriquesilva@gmail.com>
2021-02-09 20:13:07 +0100

    Updating name of file and class

--- dev/blissful/lib/screens/onboard/on_boarding_screen.dart
+++ dev/blissful/lib/screens/onboard/on_boarding_screen.dart
@@ -2,7 +2,7 @@ import 'package:blissful/components/body.dart';
 import 'package:blissful/size_config.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class OnBoardingScreen extends StatelessWidget {
   static String routeName = '/splash';
   @override
   Widget build(BuildContext context) {
