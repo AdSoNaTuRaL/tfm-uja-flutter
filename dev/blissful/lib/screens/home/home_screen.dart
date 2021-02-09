7c4236bce626039c1cecba76671ee2934ee9f9bd
Adson Henrique Moreira da Silva <adsonhenriquesilva@gmail.com>
2021-02-09 20:13:16 +0100

    Creating new screen

---
+++ dev/blissful/lib/screens/home/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = '/home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
