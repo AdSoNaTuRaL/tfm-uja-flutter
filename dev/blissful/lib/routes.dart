8cbd056a454a681d2ec9ab4f598bc0ae10d97479
Adson Henrique Moreira da Silva <adsonhenriquesilva@gmail.com>
2021-02-10 22:23:34 +0100

    Creating new route to create orphanage

--- dev/blissful/lib/routes.dart
+++ dev/blissful/lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:blissful/screens/home/home_screen.dart';
 import 'package:blissful/screens/onboard/on_boarding_screen.dart';
+import 'package:blissful/screens/orphanage/create_orphanage_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   OnBoardingScreen.routeName: (context) => OnBoardingScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
+  CreateOrphanage.routeName: (context) => CreateOrphanage(),
 };
