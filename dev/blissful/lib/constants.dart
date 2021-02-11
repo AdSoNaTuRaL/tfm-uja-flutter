086a79b50fb3fd8781369808e23d01ac3624d10e
Adson Henrique Moreira da Silva <adsonhenriquesilva@gmail.com>
2021-02-11 21:05:16 +0100

    Deleting some stuffs

--- dev/blissful/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bPrimaryColor = Color(0xFF15C3D6);
-const bPrimaryColorLight = Color(0xFFD1EDF2);
-const bSecondaryColor = Color(0xFFFFD666);
-const bTextColor = Color(0xFF0089A5);
-
-const bAnimationDuration = Duration(milliseconds: 200);
--- dev/blissful/lib/screens/orphanage/create_orphanage_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CreateOrphanage extends StatelessWidget {
-  static String routeName = '/create-orphanage';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
