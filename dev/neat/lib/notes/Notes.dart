e7933831d6bfb02673fb18aa601ef3f8b5d96206
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-05 18:47:55 +0100

    fix: notes model -> load data from db on constructor

--- dev/neat/lib/notes/Notes.dart
+++ dev/neat/lib/notes/Notes.dart
@@ -1,3 +1,4 @@
+import 'package:Neat/notes/NotesDBWorker.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'NotesEntry.dart';
@@ -5,16 +6,22 @@ import 'NotesModel.dart' show NotesModel, notesModel;
 import 'NotesList.dart';
 
 class Notes extends StatelessWidget {
+  Notes() {
+    notesModel.loadData(NotesDBWorker.db);
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModel<NotesModel>(
-        model: notesModel,
-        child: ScopedModelDescendant<NotesModel>(
-            builder: (BuildContext context, Widget child, NotesModel model) {
+      model: notesModel,
+      child: ScopedModelDescendant<NotesModel>(
+        builder: (BuildContext context, Widget child, NotesModel model) {
           return IndexedStack(
             index: model.stackIndex,
             children: <Widget>[NotesList(), NotesEntry()],
           );
-        }));
+        },
+      ),
+    );
   }
 }
